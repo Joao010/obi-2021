7e7bd52a849c987b876c91ffd0a2ebf89b7dcf7d
João Pedro <54006291+Joao010@users.noreply.github.com>
2021-11-06 21:43:27 -0300

    Create Pesquisa de preços.cpp

---
+++ Resoluções Off/Fase 2 B/Pesquisa de preços.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+
+#define vt vector
+#define eb emplace_back
+#define pb push_back
+#define f(i, s, f) for(int i = s; i < f; ++i)
+#define fd(i, s, f) for(int i = s; i >= f; --i)
+#define r(x, ns) for(auto& x : ns)
+#define s(x) int(x.size())
+#define be(ns) ns.begin(), ns.end()
+#define rbe(ns) ns.rbegin(), ns.rend()
+#define gr(type) vt<type>, greater<type>
+#define F first
+#define S second
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const int MAX = 102;
+
+int main() {
+  int n; cin>>n;
+  string a;
+  double b, c;
+  vt<string> ans;
+  f(i,0,n) {
+    cin>>a>>b>>c;
+    if(b <= (c*0.7)) ans.eb(a);
+  }
+  if(!s(ans)) cout<<"*\n";
+  r(v, ans) cout<<v<<'\n';
+}
