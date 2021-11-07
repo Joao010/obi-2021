242d0e963a1b03ea64fe8950b11fe0fcf2a9efa3
João Pedro <54006291+Joao010@users.noreply.github.com>
2021-11-06 21:40:46 -0300

    Create Recorde.cpp

---
+++ Resoluções Off/Fase 2 B/Recorde.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+
+#define vt vector
+#define eb emplace_back
+#define pb push_back
+#define f(i, s, f) for(int i = s; i < f; ++i)
+#define fd(i, s, f) for(int i = s; i >= f; --i)
+#define r(x, ns) for(auto& x : ns)
+#define s(x) int(x.size())
+#define be(ns) ns.begin(), ns.end()
+#define rbe(ns) ns.rbegin(), ns.rend()
+#define gr(type) vt<type>, greater<type>
+#define F first
+#define S second
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const int MAX = 102;
+
+int main() {
+  int a, b, c; cin>>a>>b>>c;
+  cout<<(a < b ? "RM\n" : "*\n")<<(a < c ? "RO\n" : "*\n");
+}
